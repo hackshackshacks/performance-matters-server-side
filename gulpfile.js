110c19e26f691e8b6acb4fa75d2c42f26a020d4a
Koos Bavinck <>
2018-03-20 14:22:03 +0100

    include nodemon in build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 const babel = require('gulp-babel')
+const nodemon = require('gulp-nodemon')
+const connect = require('gulp-connect')
+const open = require('gulp-open')
 
 const sources = {
   sass: 'build/src/styles/**/*.scss',
@@ -25,6 +28,7 @@ gulp.task('sass', () => {
   return gulp.src('build/src/styles/main.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest(`${outputDir}/css`))
+    .pipe(connect.reload())
 })
 
 // Convert to es5, uglify and move js files
@@ -34,6 +38,7 @@ gulp.task('js', () => {
       presets: ['es2015']
     }))
     .pipe(gulp.dest(`${outputDir}/js`))
+    .pipe(connect.reload())
 })
 
 // Watch html, sass and js
@@ -43,4 +48,35 @@ gulp.task('watch', () => {
   gulp.watch(sources.js, ['js'])
 })
 
-gulp.task('default', ['copy', 'sass', 'js', 'watch'])
+gulp.task('develop', function () {
+  var stream = nodemon({ script: 'app.js',
+    ext: 'html js css scss',
+    ignore: ['ignored.js']
+  })
+  stream
+  .on('restart', () => {
+    console.log('restarted!')
+  })
+  .on('crash', () => {
+    console.error('Application has crashed!\n')
+    stream.emit('restart', 10)  // restart the server in 10 seconds
+  })
+})
+
+// Start server
+gulp.task('connect', () => {
+  connect.server({
+    name: 'App',
+    root: 'app',
+    port: 8001,
+    livereload: true
+  })
+})
+
+// Open browser
+gulp.task('open', () => {
+  gulp.src('')
+  .pipe(open({uri: 'http://localhost:8001'}))
+})
+
+gulp.task('default', ['copy', 'sass', 'js', 'watch', 'develop', 'connect'])
